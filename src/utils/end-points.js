26c77fd4f1adeded466751186613d3f73efab371
Daniel Goss <daniilgoss@gmail.com>
2020-07-19 13:22:40 -0700

    Update end-points.js

--- src/utils/end-points.js
+++ src/utils/end-points.js
@@ -5,4 +5,4 @@ export const timeSeriesQuote = (symbol, outputSize) =>
   `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${symbol}&${outputSize}=full&apikey=${process.env.REACT_APP_API_KEY}`;
 
 export const searchQuote = (keyword) =>
-  `https://www.alphavantage.co/query?function='SYMBOL_SEARCH'&keywords=${keyword}&apikey=${process.env.REACT_APP_API_KEY}`;
+  `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${keyword}&apikey=${process.env.REACT_APP_API_KEY}`;
