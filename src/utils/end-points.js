9ff17336ab160b8a7a7f879290822a5fc890b988
Daniel Goss <daniilgoss@gmail.com>
2020-07-19 13:22:21 -0700

    Update end-points.js

--- src/utils/end-points.js
+++ src/utils/end-points.js
@@ -1,20 +1,8 @@
-const url = (quoteType, symbol) =>
-  `https://www.alphavantage.co/query?function=${quoteType}&symbol=${symbol}&apikey=${process.env.REACT_APP_API_KEY}`;
+export const singleQuote = (symbol) =>
+  `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${process.env.REACT_APP_API_KEY}`;
 
-const timeSeriesUrl = (quoteType, symbol, outputSize) =>
-  `https://www.alphavantage.co/query?function=${quoteType}&symbol=${symbol}&${outputSize}=full&apikey=${process.env.REACT_APP_API_KEY}`;
+export const timeSeriesQuote = (symbol, outputSize) =>
+  `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${symbol}&${outputSize}=full&apikey=${process.env.REACT_APP_API_KEY}`;
 
-const searchUrl = (quoteType, keyword) =>
-  `https://www.alphavantage.co/query?function=${quoteType}&keywords=${keyword}&apikey=${process.env.REACT_APP_API_KEY}`;
-
-export const singleQuote = (symbol) => {
-  return url('GLOBAL_QUOTE', symbol);
-};
-
-export const timeSeriesQuote = (symbol, outputSize) => {
-  return timeSeriesUrl('TIME_SERIES_DAILY_ADJUSTED', symbol, outputSize);
-};
-
-export const searchQuote = (keyword) => {
-  return searchUrl('SYMBOL_SEARCH', keyword);
-};
+export const searchQuote = (keyword) =>
+  `https://www.alphavantage.co/query?function='SYMBOL_SEARCH'&keywords=${keyword}&apikey=${process.env.REACT_APP_API_KEY}`;
