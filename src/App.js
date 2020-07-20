11b36dc430d399b3f3ac3b548c989bdc3b6956d2
Daniel Goss <daniilgoss@gmail.com>
2020-07-19 17:09:31 -0700

    more stuff

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
 import SingleQuote from './components/single-quote';
-// import Search from './components/search';
-import TimeSeries from './components/time-series';
+import Search from './components/search';
+// import TimeSeries from './components/time-series';
 
 function App() {
   return (
     <div className='App-header'>
       <SingleQuote />
-      {/* <Search /> */}
-      <TimeSeries />
+      <Search />
+      {/* <TimeSeries /> */}
     </div>
   );
 }
--- src/components/search.js
+++ src/components/search.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import Table from './shared/table';
-import { searchQuote } from '../utils/end-points';
+import { searchQuote, singleQuote } from '../utils/end-points';
 import { formatSingleQuote } from '../utils/formatters';
+import SearchField from 'react-search-field';
 import '../table.css';
 import '../single-quote.css';
 
@@ -14,59 +15,89 @@ export default class Search extends Component {
       value: '',
       isLoaded: false,
       error: false,
+      isSearching: false,
     };
   }
 
-  search = (event) => {
-    console.log(this.state.value);
+  onEnterChange = () => {
+    if (this.state.value.length === 0) return;
+    fetch(singleQuote(this.state.value))
+      .then((response) => {
+        return response.json();
+      })
+      .then((parsedJson) => {
+        // if (JSON.stringify(parsedJson) === '{"Global Quote":{}}')
+        //   this.setState({
+        //     // error: true,
+        //     // ticker: { invalidValue: this.state.value },
+        //     // isSearching: false,
+        //   });
+        // else {
+
+        this.setState({
+          ticker: {
+            validValue: formatSingleQuote(parsedJson),
+          },
+          isSearching: false,
+          isLoaded: true,
+        });
+      });
+  };
+
+  search = () => {
+    // console.log(this.state.value);
     fetch(searchQuote(this.state.value))
       .then((response) => {
         return response.json();
       })
       .then((parsedJson) => {
-        console.log(4, Object.keys(parsedJson)[0]);
-        if (Object.keys(parsedJson)[0] === 'Error Message')
-          this.setState({
-            ticker: { invalidValue: this.state.value },
-            // error: true,
-            // isLoaded: false,
-          });
-        else {
-          console.log(1, parsedJson);
-          this.setState({
-            ticker: parsedJson,
-            // error: false,
-            isLoaded: true,
-          });
-        }
+        // console.log(4, Object.keys(parsedJson)[0]);
+        // if (Object.keys(parsedJson)[0] === 'Error Message')
+        //   this.setState({
+        //     error: true,
+        //   });
+        // else {
+        //   console.log(1, parsedJson);
+        //   this.setState({
+        //     error: false,
+        //     isLoaded: true,
+        //     ticker: parsedJson,
+        //   });
+        // }
+        this.setState({
+          ticker: { searchResults: parsedJson },
+          isSearching: true,
+        });
       });
-    event.preventDefault();
   };
 
-  handleChange = (event) => {
-    this.setState({ value: event.target.value });
-    this.search(event);
+  onSearchChange = (event) => {
+    this.setState({ value: event });
+    this.search();
   };
 
-  tickerExists = () => {
-    return this.state.isLoaded;
-  };
-
-  renderInvalidSelection = () => {
-    return (
-      <h5>
-        "{this.state.ticker.invalidValue}" is not a valid selection. Please
-        enter a valid ticker and try again
-      </h5>
-    );
-  };
+  //   renderInvalidSelection = () => {
+  //     return <h5>"{this.state.value}" did not yield any search results</h5>;
+  //   };
 
   renderTicker = () => {
     return (
       <h6>
-        {this.tickerExists()
-          ? JSON.stringify(this.state.ticker)
-          : this.state.value}
+        {this.state.isLoaded ? (
+          <Table value={this.state.ticker.validValue} />
+        ) : (
+          ''
+        )}
+      </h6>
+    );
+  };
+
+  renderSearchResults = () => {
+    return (
+      <h6>
+        {this.state.isSearching
+          ? JSON.stringify(this.state.ticker.searchResults)
+          : ''}
       </h6>
     );
   };
@@ -75,22 +106,15 @@ export default class Search extends Component {
     console.log(this.state.value);
     return (
       <div id='quote'>
-        {/* <Header /> */}
-        {/* <form onSubmit={this.search}> */}
-        <form>
-          <label>
-            <h2>Search for a Quote</h2>
-            <input
-              placeholder='Enter Symbol'
-              type='text'
-              value={this.state.value}
-              onChange={this.handleChange}
-            ></input>
-          </label>
-        </form>
-        {this.state.ticker.invalidValue
-          ? this.renderInvalidSelection()
-          : this.renderTicker()}
+        <SearchField
+          placeholder='Search for a Symbol'
+          onChange={(this.state.value, this.onSearchChange)}
+          onEnter={(this.state.value, this.onEnterChange)}
+          onSearchClick={this.onEnterChange}
+          classNames='test-class'
+        />
+        {this.renderSearchResults()}
+        {this.renderTicker()}
       </div>
     );
   }
